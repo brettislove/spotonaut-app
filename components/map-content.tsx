51806ebc37ecd3719323b720bea6858fe4dfb340
Bretislav Dancak <uvqeg@student.kit.edu>
2025-11-22 15:36:42 +0100

    Changed basemap of the map

--- components/map-content.tsx
+++ components/map-content.tsx
@@ -38,8 +38,10 @@ export default function MapContent({ position, location }: MapContentProps) {
       scrollWheelZoom={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
+        maxZoom={20}
       />
       <Marker position={position}>
         <Popup>
